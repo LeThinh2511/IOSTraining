6464a02b00d0b977aa68a93aa921056f09f7d6ac
LeThinh2511 <thinhlefrs@gmail.com>
2018-02-26 20:19:28 +0700

    programatic control

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -13,6 +13,20 @@ class MapViewController: UIViewController
 {
     var mapView: MKMapView!
     
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl)
+    {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
     override func loadView() {
         mapView = MKMapView()
         view = mapView
@@ -24,16 +38,23 @@ class MapViewController: UIViewController
         
         segmentedControl.selectedSegmentIndex = 0
         
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(segmentedControl)
         
         let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
-        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
-        let trailingconstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingconstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
         topConstraint.isActive = true
         leadingConstraint.isActive = true
         trailingconstraint.isActive = true
     }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view.")
