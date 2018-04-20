c6b55a65d396abb88d6b602c1514b6934b96ec89
LeThinh2511 <thinhlefrs@gmail.com>
2018-04-20 11:25:52 +0700

    - Drawing with DrawView

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by ThinhLe on 4/20/18.
+//  Copyright © 2018 ThinhLe. All rights reserved.
+//
+
+import CoreGraphics
+
+struct Line
+{
+    var begin = CGPoint.zero
+    var end =  CGPoint.zero
+}
