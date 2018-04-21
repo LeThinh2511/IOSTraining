c3d29d7ad03e618030487614963286c61616c280
LeThinh2511 <thinhlefrs@gmail.com>
2018-04-21 15:08:57 +0700

    -[Photorama]: Building URL

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by ThinhLe on 4/21/18.
+//  Copyright © 2018 ThinhLe. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController
+{
+    @IBOutlet var imageView: UIImageView!
+//    //method": "flickr.interestingness.getList
+//    api_key": "a6d819499131071f158fd740860a5a88
+//    extras": "url_h,date_taken
+//    format": "json
+//    nojsoncallback": "1
+//https://api.flickr.com/services/rest
+}
--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class DrawView: UIView
 {
-    var currentLine: Line?
+    var currentLines = [NSValue:Line]()
     var finishedLines = [Line]()
     
     func stroke(_ line: Line)
     {
+        print(#function)
         let path = UIBezierPath()
         path.lineWidth = 10
         path.lineCapStyle = .round
@@ -25,16 +26,60 @@ class DrawView: UIView
     }
     
     override func draw(_ rect: CGRect) {
+        print(#function)
         UIColor.black.setStroke()
         for line in finishedLines
         {
             stroke(line)
         }
         
-        if let line = currentLine
+        UIColor.red.setStroke()
+        for (_ , line) in currentLines
         {
-            UIColor.red.setStroke()
             stroke(line)
         }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        for touch in touches
+        {
+            let location = touch.location(in: self)
+            let newLine = Line(begin: location, end: location)
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key] = newLine
+        }
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        for touch in touches
+        {
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key]?.end = touch.location(in: self)
+        }
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        for touch in touches
+        {
+            let key = NSValue(nonretainedObject: touch)
+            if var line = currentLines[key]
+            {
+                line.end = touch.location(in: self)
+                finishedLines.append(line)
+                currentLines.removeValue(forKey: key)
+            }
+        }
+        setNeedsDisplay()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        currentLines.removeAll()
+        setNeedsDisplay()
     }
 }
